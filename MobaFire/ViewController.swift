393793432e705a665769d0e7dc77dbd96ba01353
George <soysmile@outlook.com>
2018-08-22 22:48:34 +0300

    title fix

---
+++ MobaFire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobaFire
+//
+//  Created by George Heints on 11.01.2018.
+//  Copyright © 2018 George Heints. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
